76ee1766dad84f627db141a41136097a4e748f05
Ayush <ayushaswal583@gmail.com>
2025-02-10 20:00:02 +0530

    links added & css improved

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,7 +12,7 @@ const navigation = [
     },
     {
         name: 'GitHub',
-        href: 'https://github.com/',
+        href: 'https://github.com/Ayush583-Aswal',
         icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
             <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
                 <path d='M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.66-.21.66-.47v-1.72c-2.78.6-3.37-1.34-3.37-1.34-.45-1.15-1.1-1.45-1.1-1.45-.9-.62.07-.6.07-.6 1 .07 1.53 1.04 1.53 1.04.88 1.52 2.3 1.08 2.86.83.09-.64.34-1.08.62-1.33-2.22-.25-4.56-1.11-4.56-4.95 0-1.09.39-1.98 1.03-2.68-.1-.25-.45-1.26.1-2.62 0 0 .84-.27 2.75 1.02a9.62 9.62 0 0 1 5 0c1.91-1.29 2.75-1.02 2.75-1.02.55 1.36.2 2.37.1 2.62.64.7 1.03 1.59 1.03 2.68 0 3.86-2.35 4.7-4.59 4.95.35.3.66.91.66 1.84v2.73c0 .27.16.57.67.47A10.02 10.02 0 0 0 22 12c0-5.52-4.48-10-10-10z'/>
@@ -21,7 +21,7 @@ const navigation = [
     },
     {
         name: 'X',
-        href: 'https://twitter.com/',
+        href: 'https://x.com/Ayush5831?t=JVRa5mL-7cHiHep65cFHQw&s=09',
         icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
             <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
                 <path d='M23 3a10.9 10.9 0 0 1-3.14 1.53A4.48 4.48 0 0 0 22.46 2a9 9 0 0 1-2.88 1.11 4.52 4.52 0 0 0-7.86 3.1 4.49 4.49 0 0 0 .11 1.03A12.8 12.8 0 0 1 2 3.5a4.51 4.51 0 0 0-.61 2.27 4.48 4.48 0 0 0 2 3.72 4.48 4.48 0 0 1-2-.55v.06a4.5 4.5 0 0 0 3.6 4.4 4.5 4.5 0 0 1-2 .08 4.51 4.51 0 0 0 4.21 3.14A9.05 9.05 0 0 1 2 18.41 12.81 12.81 0 0 0 9.29 21c8.47 0 13.11-7 13.11-13.07q0-.3 0-.6A9.35 9.35 0 0 0 23 3z'/>
@@ -31,7 +31,7 @@ const navigation = [
     },
     {
         name: 'Instagram',
-        href: 'https://www.instagram.com/',
+        href: 'https://www.instagram.com/ayushaswal583?igsh=MWl0MTU0Y3UwNXlldg==',
         icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
             <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
                 <path d='M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9A5.5 5.5 0 0 1 16.5 22h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2zm0-2A7.5 7.5 0 0 0 0 7.5v9A7.5 7.5 0 0 0 7.5 24h9A7.5 7.5 0 0 0 24 16.5v-9A7.5 7.5 0 0 0 16.5 0h-9zM12 7a5 5 0 1 1-5 5 5 5 0 0 1 5-5zm0-2A7 7 0 1 0 19 12a7 7 0 0 0-7-7zm6.5 1.5A1.5 1.5 0 1 1 17 7a1.5 1.5 0 0 1 1.5-1.5z'/>
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,87 +1,89 @@
-  "use client";
-  import React from "react";
-  import Image from "next/image";
-  import { TypeAnimation } from "react-type-animation";
-  import { motion } from "framer-motion";
-  import Link from "next/link";
-  import currentPhoto from "@/lib/images/currentphoto.jpg";
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import currentPhoto from "@/lib/images/currentphoto.png";
 
-  const HeroSection = () => {
-    return (
-      <section id="about" className="lg:py-16">
-        <div className="grid grid-cols-1 sm:grid-cols-12">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-          >
-            <h1 className="mb-4 text-lg sm:text-xl lg:text-7xl lg:leading-normal font-extrabold">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600">
-                Hello, I&apos;m{" "}
+const HeroSection = () => {
+  return (
+    <section id="about" className="lg:py-16 py-10 flex flex-col items-center text-center">
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-6 lg:gap-12 ">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-center lg:text-left max-w-xl "
+        >
+          <h1 className="mb-4 text-2xl sm:text-3xl lg:text-6xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Ayush",
+                1000,
+                "Web Developer",
+                1000,
+                "Full-Stack Dev",
+                1000,
+                "Software Engineer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#777] text-base sm:text-lg mb-6 lg:text-xl">
+            Based in India. I am passionate about learning new technologies and building 
+            web applications. I also have little experience in Machine Learning and Data Science. 
+          </p>
+
+          <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4">
+            <Link
+              href="#contact"
+              onClick={(e) => {
+                e.preventDefault();
+                document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });
+              }}
+              className="px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-blue-700 hover:bg-blue-600 text-white text-center"
+            >
+              Hire Me
+            </Link>
+
+            <Link
+              href="https://drive.google.com/file/d/1guJN6y6WKFeLlmMgGfcL2rozK-laGs9D/view?usp=drive_link"
+              className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-blue-700 hover:bg-blue-800 text-white text-center"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
               </span>
-              <br></br>
-              <TypeAnimation
-                sequence={[
-                  "Ayush",
-                  1000,
-                  "Web Developer",
-                  1000,
-                  "Full-Stack Dev",
-                  1000,
-                  "Software Engineer",
-                  1000,
-                ]}
-                wrapper="span"
-                speed={50}
-                repeat={Infinity}
-              />
-            </h1>
-            <p className="text-[#777] text-base sm:text-lg mb-6 lg:text-xl">
-              based in India. I am passionate about learning new technologies and building 
-              web applications. I also have little experience on Machine Learning and Data Science. 
-            </p>
-            <div>
-              <Link
-                href="#contact"
-                onClick={(e) => {
-                  e.preventDefault();
-                  document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });
-                }}
-                className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-700 hover:bg-blue-600 text-white"
-              >
-                Hire Me
-              </Link>
+            </Link>
+          </div>
+        </motion.div>
 
-              <Link
-                href="https://drive.google.com/file/d/1guJN6y6WKFeLlmMgGfcL2rozK-laGs9D/view?usp=drive_link"
-                className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-blue-700 hover:bg-blue-800 text-white mt-3"
-              >
-                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                  Download CV
-                </span>
-              </Link>
-            </div>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="col-span-4 place-self-center mt-4 lg:mt-0"
-          >
-            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-              <Image
-                src={currentPhoto} 
-                alt="hero image"
-                className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                width={300}
-                height={300}
-              />
-            </div>
-          </motion.div>
-        </div>
-      </section>
-    );
-  };
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="rounded-full bg-[#181818] w-[180px] h-[180px] sm:w-[250px] sm:h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src={currentPhoto}
+              alt="hero image"
+              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={380}
+              height={380}
+            />
+          </div>
+        </motion.div>
 
-  export default HeroSection;
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
